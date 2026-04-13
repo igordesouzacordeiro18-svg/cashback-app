1af0b79de3f2749ee7b495bb9871a883331c0d00
igor cordeiro <igordesouzacordeiro18@gmail.com>
2026-04-13 18:19:48 -0300

    fix deploy without mysql

--- db.py
+++ db.py
@@ -1,43 +1,7 @@
-import mysql.connector
-
-def conectar():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="cashback_db"
-    )
-
+dados_fake = []
 
 def salvar_consulta(ip, tipo_cliente, valor, cashback):
-    conexao = conectar()
-    cursor = conexao.cursor()
-
-    sql = "INSERT INTO consultas (ip, tipo_cliente, valor, cashback) VALUES (%s, %s, %s, %s)"
-    valores = (ip, tipo_cliente, valor, cashback)
-
-    cursor.execute(sql, valores)
-    conexao.commit()
-
-    cursor.close()
-    conexao.close()
+    dados_fake.append((ip, tipo_cliente, valor, cashback))
 
 def buscar_historico(ip):
-    conexao = conectar()
-    cursor = conexao.cursor()
-
-    sql = """
-    SELECT tipo_cliente, valor, cashback 
-    FROM consultas 
-    WHERE ip = %s 
-    ORDER BY id DESC 
-    LIMIT 10
-    """
-    
-    cursor.execute(sql, (ip,))
-    dados = cursor.fetchall()
-
-    cursor.close()
-    conexao.close()
-
-    return dados
+    return [d[1:] for d in dados_fake if d[0] == ip][-10:]
